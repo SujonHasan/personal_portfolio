d0133a92cdeda4f7a3086bc689d1f338a679f153
SujonHasan <sujonhasan171@gmail.com>
2023-11-07 22:05:30 +0600

    code modify and error code added

--- src/Containers/Shared/Footer/Footer.js
+++ src/Containers/Shared/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="footer footer-center p-4 text-base-content mt-10 text-[20px] text-gray-500 font-serif">
             <aside>
                 <p>Thanks for visiting my portfolio</p>
-                <p>Copyright © 2023 - All right reserved Sujon Hasan</p>
+                <p>Copyright © 2023 <span className='font-sans' >Sujon Hasan</span> </p>
             </aside>
         </footer>
     );
